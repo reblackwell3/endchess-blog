995234df0c965082fdf6f134e678333b4ca1d95f
Robert Blackwell <reblackwell3@gmail.com>
2024-07-14 12:07:35 -0400

    base react project

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = ({ title, content }) => {
+  return (
+    <div className="post">
+        <h2>{title}</h2>
+        <p>{content}</p>
+    </div>  
+  );  
+};
+
+export default Post;
