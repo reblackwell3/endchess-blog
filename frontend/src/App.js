81dcb1fdddbe43a1ac153f5bf3c82a973e60ffe8
Robert Blackwell <reblackwell3@gmail.com>
2024-07-30 15:42:37 -0400

    links to home and about me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AllPosts from './posts/Post'
-import Navbar from './Navbar'
+import Navbar from './navbar/Navbar'
+import About from './about/About'
+import AppRoutes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <AllPosts />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <AppRoutes />
+        </main>
+      </div>
+    </Router>
   );
 }
 
