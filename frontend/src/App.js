81593a964353deb7891796acba99ca93ba1a15d5
Robert Blackwell <reblackwell3@gmail.com>
2024-07-30 16:08:53 -0400

    simplify files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import AllPosts from './posts/Post'
-import Navbar from './navbar/Navbar'
-import About from './about/About'
+import AllPosts from './components/Post'
+import Navbar from './components/Navbar'
+import About from './components/About'
 import AppRoutes from './routes'
 
 function App() {
