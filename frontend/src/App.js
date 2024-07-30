edf41e71a3a23f2f21478961626bad3dda185f1e
Robert Blackwell <reblackwell3@gmail.com>
2024-07-30 11:52:01 -0400

    navbar without routing to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AllPosts from './posts/Post'
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <main>
         <AllPosts />
       </main>
