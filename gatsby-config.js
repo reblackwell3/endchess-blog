b3da520c0455fdae8124ed8441d59418d878bc99
Robert Blackwell <reblackwell3@gmail.com>
2024-08-16 12:25:06 -0400

    update site info and bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Endchess Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Robert Blackwell`,
+      summary: `who codes endchess by day and blogs endchess by night.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog showcasing how endchess was coded and chess concepts.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `blackfromchina`,
     },
   },
   plugins: [
